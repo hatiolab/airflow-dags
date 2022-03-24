cf8ba18792cdd9506f18cc69ce39403d344f8d83
Jinwon <41100208+vincent841@users.noreply.github.com>
2022-03-24 17:57:36 +0900

    Update sync_all_market_place_channel_products.py

--- wms/sync_all_market_place_channel_products.py
+++ wms/sync_all_market_place_channel_products.py
@@ -8,41 +8,37 @@ from airflow.models.variable import Variable
 
 # python task functions
 def sync_all_market_place_channel_products(host_url, access_token) -> None:
-    try:
-        # TODO: change graphql mutation with variables to be run on schedule
-        graphql_mutation = """
-            mutation syncAllMarketplaceChannelProducts {
-                syncAllMarketplaceChannelProducts
-            }
-        """
-
-        # extract a domain name from the access token
-        decoded_token = jwt.decode(access_token, options={"verify_signature": False})
-        things_factory_domain = decoded_token.get("domain").get("subdomain")
-
-        # set http url with headers(access token and things factory domain on multi-domain configuation)
-        reqHeaders = {
-            "authorization": access_token,
-            "x-things-factory-domain": things_factory_domain,
+    # TODO: change graphql mutation with variables to be run on schedule
+    graphql_mutation = """
+        mutation syncAllMarketplaceChannelProducts {
+            syncAllMarketplaceChannelProducts
         }
+    """
 
-        # create http transport
-        _transport = RequestsHTTPTransport(
-            url=f"{host_url}/graphql",
-            headers=reqHeaders,
-            use_json=True,
-        )
+    # extract a domain name from the access token
+    decoded_token = jwt.decode(access_token, options={"verify_signature": False})
+    things_factory_domain = decoded_token.get("domain").get("subdomain")
 
-        # create grapql client
-        client = GqlClient(
-            transport=_transport,
-            fetch_schema_from_transport=False,
-        )
+    # set http url with headers(access token and things factory domain on multi-domain configuation)
+    reqHeaders = {
+        "authorization": access_token,
+        "x-things-factory-domain": things_factory_domain,
+    }
 
-        # execute the mutation
-        m = gql(graphql_mutation)
-        client_result = client.execute(m)
-        print("mutation execution result: ", client_result)
+    # create http transport
+    _transport = RequestsHTTPTransport(
+        url=f"{host_url}/graphql",
+        headers=reqHeaders,
+        use_json=True,
+    )
 
-    except Exception as ex:
-        print("Exception: ", ex)
+    # create grapql client
+    client = GqlClient(
+        transport=_transport,
+        fetch_schema_from_transport=False,
+    )
+
+    # execute the mutation
+    m = gql(graphql_mutation)
+    client_result = client.execute(m)
+    print("mutation execution result: ", client_result)
