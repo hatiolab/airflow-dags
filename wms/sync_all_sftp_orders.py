b6e2470e00bd53abe4a5a67e795820f5d11b2918
sumjoekin <joekin95@gmail.com>
2022-05-05 15:43:11 +0800

    print more info

--- wms/sync_all_sftp_orders.py
+++ wms/sync_all_sftp_orders.py
@@ -19,6 +19,13 @@ def sync_all_sftp_orders(
     decoded_token = jwt.decode(access_token, options={"verify_signature": False})
     things_factory_domain = decoded_token.get("domain").get("subdomain")
 
+    print("---------------------------------------------------------------")
+    print("[None] decoded_token: ", decoded_token)
+    print("[None] things_factory_domain: ", things_factory_domain)
+    print("[None] customer_domain_id: ", customer_domain_id)
+    print("[None] access_token: ", access_token)
+    print("---------------------------------------------------------------")
+
     # TODO: change graphql mutation with variables to be run on schedule
     graphql_mutation = """
         mutation syncSftpOrders($customerDomainId: String!) {
@@ -32,6 +39,13 @@ def sync_all_sftp_orders(
     # extract a domain name from the access token
     decoded_token = jwt.decode(access_token, options={"verify_signature": False})
     things_factory_domain = decoded_token.get("domain").get("subdomain")
+
+    print("---------------------------------------------------------------")
+    print("[Params] decoded_token: ", decoded_token)
+    print("[Params] things_factory_domain: ", things_factory_domain)
+    print("[Params] customer_domain_id: ", customer_domain_id)
+    print("[Params] access_token: ", access_token)
+    print("---------------------------------------------------------------")
 
     # set http url with headers(access token and things factory domain on multi-domain configuation)
     reqHeaders = {
