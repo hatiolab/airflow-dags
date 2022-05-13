f68110418e487b44c936a4529723a167de0e680d
sumjoekin <joekin95@gmail.com>
2022-05-13 16:40:35 +0800

    update avern variables

--- dag_sync_avern_sellercraft_qty.py
+++ dag_sync_avern_sellercraft_qty.py
@@ -37,9 +37,9 @@ with DAG(
 
         try:
             host_url = Variable.get("OPERATO_COREAPP_URL")
-            access_token = Variable.get("AVERN_ACCESS_TOKEN")
-            sellercraft_id = Variable.get("AVERN_SELLERCRAFT_ID")
-            fulfillment_center_id = Variable.get("AVERN_FULFILLMENT_CENTER_ID")
+            access_token = Variable.get("OPERATO_AVERN_ACCESS_TOKEN")
+            sellercraft_id = Variable.get("OPERATO_AVERN_SELLERCRAFT_ID")
+            fulfillment_center_id = Variable.get("OPERATO_AVERN_FULFILLMENT_CENTER_ID")
 
             sync_avern_sellercraft_qty(host_url, access_token, sellercraft_id, fulfillment_center_id)
 
